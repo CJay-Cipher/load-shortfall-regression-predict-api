2185cf9d75501b1e6badb14a949f104a4150d774
CJay <chijiokennabueze@gmail.com>
2023-05-24 18:06:39 +0100

    update

--- utils/request.py
+++ utils/request.py
@@ -37,7 +37,7 @@ feature_vector_json = test.iloc[1].to_json()
 # replace the URL below with its public IP:
 
 # url = 'http://{public-ip-address-of-remote-machine}:5000/api_v0.1'
-url = 'http://176.34.172.199:5000/api_v0.1'
+url = 'http://52.213.6.231:5000/api_v0.1'
 
 # url = 'http://127.0.0.1:5000/api_v0.1'
 
